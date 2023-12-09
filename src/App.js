0d34151f65934f494a16eaa765d769ebb71a04ab
Soichiro Inatani <inai17ibar@gmail.com>
2023-12-09 19:11:34 +0900

    Cookieからサーバーでのセッション認証に変更

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { useCookies } from 'react-cookie';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [cookies, setCookie, removeCookie] = useCookies(['user']);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = () => {
-    setCookie('user', 'john', { path: '/' });
+    axios.post('http://localhost:8080/login', {}, { withCredentials: true })
+      .then(response => {
+        console.log(response.data);
+        setIsLoggedIn(true);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   };
 
   const handleLogout = () => {
-    removeCookie('user');
+    axios.post('http://localhost:8080/logout', {}, { withCredentials: true })
+      .then(() => setIsLoggedIn(false))
+      .catch(err => console.error(err));
+  };
+
+  const checkSecret = () => {
+    console.log("checkSecret is called");
+    axios.get('http://localhost:8080/secret', { withCredentials: true }) //これがひつようだった
+      .then(response => console.log(response.data))
+      .catch(err => console.error(err));
   };
 
   return (
     <div>
-      {cookies.user ? (
-        <button onClick={handleLogout}>Logout</button>
+      {isLoggedIn ? (
+        <>
+          <button onClick={handleLogout}>Logout</button>
+          <button onClick={checkSecret}>Check Secret</button>
+        </>
       ) : (
         <button onClick={handleLogin}>Login</button>
       )}
